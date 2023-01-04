cdb2550b309dbada4aa1c8a79ee2321bda69249b
tortasts <sts19@alu.ua.es>
2023-01-04 19:30:43 +0100

    sound component sound

---
+++ src/cmp/soundcomponent.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include "component.hpp"
+extern "C"{
+    #include <FMOD/src/fmod_errors.h>
+}
+#include <FMOD/src/fmod.hpp>
+#include <FMOD/src/fmod_studio.hpp>
+#include <FMOD/src/common.hpp>
+
+struct ProgrammerSoundContext {
+    FMOD::System* coreSystem;
+    FMOD::Studio::System* system;
+    const char* dialogueString;
+};
+
+struct SoundComponent : public Component{
+    explicit SoundComponent(std::size_t eid) : Component(eid){};
+    ProgrammerSoundContext programmerSoundContext{};
+    FMOD::Studio::EventInstance* sound{};
+};
