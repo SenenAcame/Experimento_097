7439dbdc5a03eaaaffdc37fc2eb5c9197a9db668
JorgeLizonPerea <jlp40@gcloud.ua.es>
2023-01-27 16:35:53 +0100

    Arma pistola predeterminada

--- src/cmp/inventario.hpp
+++ src/cmp/inventario.hpp
@@ -3,7 +3,7 @@
 
 struct InventarioCmp {
     
-    int equipada =-1;
+    int equipada =0;
     //1 =  desbloqueada, 0 = no, 2 = arma actual
     size_t inventary [3] = {2,1,0}; //1 = pistola, 2 = escopeta, 3 = ametralladora
     //ammos and magazines
