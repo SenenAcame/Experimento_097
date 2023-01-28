99547b41c321c1018f1eb6b599901b7e295b262e
Joan Pascual <jpi9@gcloud.ua.es>
2023-01-28 12:54:04 +0100

    Solucionado un problema gordo del motor

--- src/eng/GLgEngine.hpp
+++ src/eng/GLgEngine.hpp
@@ -1,8 +1,9 @@
 #pragma once
 
-#include <glad/glad.h>
+#include "../lib/glad/src/glad.h"
 #include <GLFW/glfw3.h>
 #include <iostream>
+
 
 
 void framebuffer_size_callback(GLFWwindow*, int, int);
--- src/main.cpp
+++ src/main.cpp
@@ -102,14 +102,14 @@
 //}
 
 
-const *vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
 
-const *fragmentShaderSource = "#version 330 core\n"
+const char *fragmentShaderSource = "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
@@ -131,10 +131,10 @@ int main(){
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGl", NULL, NULL);
     if(window == NULL) throw std::runtime_error("GLFW Error creating Window");
     glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallBack(window, framebuffer_size_callback);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
     //______Initializate GLAD__________
-    if(!gladLoaderGLLoader(GLADloadproc)glfwGetProcAdress) throw std::runtime_error("Failed to initializate GLAD");
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) throw std::runtime_error("Failed to initializate GLAD");
     
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
@@ -194,7 +194,7 @@ int main(){
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
