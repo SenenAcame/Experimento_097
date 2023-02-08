2be48f89d9c57067d52c3a868bf9a7094819198a
Joan Pascual <jpi9@gcloud.ua.es>
2023-02-08 18:37:02 +0100

    hay camara y ya funciona el raton con la camara

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include "eng/shader.hpp"
+#include "eng/camara.hpp"
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -17,16 +18,10 @@ const  unsigned int SCR_WIDTH = 800;
 const  unsigned int SCR_HEIGHT = 600;
 
 //camera
-glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
-glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
-glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
-
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
-float yaw   = -90.0f;
-float pitch =  0.0f;
-float lastX =  800.0f / 2.0;
-float lastY =  600.0f / 2.0;
-float fov   =  45.0f; 
 
 //timing
 float deltaTime = 0.0f;
@@ -212,8 +207,11 @@ int main(){
         //Render container
         ourShader.use();
         
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        ourShader.setMat4("projection", projection);
+        
         //camera view transformations
-        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        glm::mat4 view = camera.GetViewMatrix();
         ourShader.setMat4("view", view);
 
 
@@ -248,17 +246,17 @@ void processInput(GLFWwindow* window){
     }
     float cameraSpeed =  static_cast<float>(2.5 * deltaTime);
     if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-        cameraPos += cameraSpeed * cameraFront;
+        camera.ProcessKeyboard(FORWARD, deltaTime);
     }
      if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
-        cameraPos -= cameraSpeed * cameraFront;
-     }
+        camera.ProcessKeyboard(BACKWARD, deltaTime);
+    }
       if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-        cameraPos -= (glm::normalize(glm::cross(cameraFront, cameraUp)) * 0.25f);
-      }
+        camera.ProcessKeyboard(LEFT, deltaTime);
+    }
        if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-        cameraPos += (glm::normalize(glm::cross(cameraFront, cameraUp)) * 0.25f);
-       }
+        camera.ProcessKeyboard(RIGHT, deltaTime);
+    }
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height){
@@ -280,31 +278,9 @@ void mouse_callback(GLFWwindow* window, double xposIn, double yposIn){
     lastX = xpos;
     lastY = ypos;
 
-    float sensivity = 0.1f;
-    offset_x *= sensivity;
-    offset_y *= sensivity;
-
-    //make sure when pitch is out of bounds, screen doesnt get flipped
-    if(pitch > 89.0f){
-        pitch = 89.0f;
-    }
-    if(pitch < -89.0f){
-        pitch = -89.0f; 
-    }
-
-    glm::vec3 front;
-    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
-    front.y = sin(glm::radians(pitch));
-    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
-    cameraFront = glm::normalize(front);
+    camera.ProcessMouseMovement(offset_x, offset_y);
 }
 
 void scroll_callback(GLFWwindow* window, double offset_x, double offset_y){
-    fov -= (float)offset_y;
-    if(fov <1.0f){
-        fov = 1.0;
-    }
-    if(fov > 45.0f){
-        fov = 45.0f;
-    }
+    camera.ProcessMouseScroll(static_cast<float>(offset_y));
 }
