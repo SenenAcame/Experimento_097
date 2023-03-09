92740ea12f327671518c4f971bb028f711a7942b
AngelicaQuaranta <aqb8@alu.ua.es>
2023-03-09 18:00:55 +0100

    carga modelos individuales de salas y pasillos, una textura de prueba

--- src/man/levelman.hpp
+++ src/man/levelman.hpp
@@ -29,7 +29,12 @@ struct LevelMan {
         Enty& map = EM.createEntity();
         EM.addComponent<PhysicsCmp2>(map);
         EM.addComponent<PhysicsCmp2>(map);
-        EM.addComponent<RenderCmp2> (map, dev.createModel("assets/models/mapa.obj","assets/textures/wall.bmp"));
+        EM.addComponent<RenderCmp2> (map, dev.createModel("assets/models/Sala_1.obj","assets/textures/textura_Sala_1.png"));
+        EM.addComponent<RenderCmp2> (map, dev.createModel("assets/models/Sala_2.obj","assets/textures/textura_Sala_1.png"));
+        EM.addComponent<RenderCmp2> (map, dev.createModel("assets/models/Sala_3.obj","assets/textures/textura_Sala_1.png"));
+        EM.addComponent<RenderCmp2> (map, dev.createModel("assets/models/Pasillo_1.obj","assets/textures/textura_Sala_1.png"));
+        EM.addComponent<RenderCmp2> (map, dev.createModel("assets/models/Pasillo_2.obj","assets/textures/textura_Sala_1.png"));
+        EM.addComponent<RenderCmp2> (map, dev.createModel("assets/models/Pasillo_3.obj","assets/textures/textura_Sala_1.png"));
         EM.addComponent<NodoCmp>    (map, NodoCmp{.salas=MapSys.creaSalas()});
         EM.addComponent<SoundCmp>   (map, SoundCmp{.programmerSoundContext=SouSys.createinstance(0), .parametro=0, .play=true});
         EM.addTag      <TMap>       (map);
